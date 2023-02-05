6f19f31e828822938afb6e73879aae9888c0c821
Oxana2332 <far_north@ro.ru>
2023-02-05 12:59:09 +0300

    start

---
+++ flashcards/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './header.css'
+
+function header() {
+    return (
+        <header className="header">English on flashcards</header>
+    )
+}
+
+export default header
---
+++ flashcards/src/components/app/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from '../Header/Header.jsx';
+import Footer from '../Footer/Footer.jsx';
+import MainPage from '../MainPage/MainPage.jsx';
+
+function App() {
+
+  return (
+    <div className="App">
+    <Header />
+    <MainPage/>
+    <Footer />
+    </div>
+  );
+}
+
+export default App;
